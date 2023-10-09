5286e768733eb1b2433488d909f8658ec937dac7
Stephane-Gon <stephane_work@outlook.com>
2023-10-09 18:46:20 +0100

    adicionadas novas calls na store dos users

--- src/store/users/service.ts
+++ src/store/users/service.ts
@@ -5,18 +5,38 @@ export const MetadataService = Api.injectEndpoints({
   endpoints: build => ({
     getAllUsers: build.query({
       query: () => `/users`,
+      providesTags: ['Users']
+    }),
+    getUser: build.query({
+      query: (id: number) => `/users/${id}`,
+      providesTags: ['Users']
     }),
     createUser: build.mutation<void, { body: Partial<CreateUserPayload> }>({
       query: ({ body }) => ({
-        url: `users/create`,
+        url: `/users/create`,
         method: 'POST',
         body: body,
       }),
+      invalidatesTags: ['Users']
+    }),
+    updateUser: build.mutation<void, { body: Partial<CreateUserPayload>, id: number}>({
+      query: ({ body, id }) => ({
+        url: `/users/update/${id}`,
+        method: 'POST',
+        body: body,
+      }),
+      invalidatesTags: ['Users']
     }),
   }),
+  
   overrideExisting: false,
 });
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetAllUsersQuery, useCreateUserMutation } = MetadataService;
+export const { 
+  useGetAllUsersQuery, 
+  useCreateUserMutation, 
+  useUpdateUserMutation,
+  useGetUserQuery
+} = MetadataService;
