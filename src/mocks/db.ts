f8ae7a4fe65a5f450e01b62b046c28168a44519b
Stephane-Gon <stephane_work@outlook.com>
2023-10-09 18:44:48 +0100

    fix do problema em que a mock api não esta a funcionar

--- src/mocks/db.ts
+++ src/mocks/db.ts
@@ -1,8 +1,8 @@
-import { factory, primaryKey } from '@mswjs/data';
+import { factory, primaryKey, } from '@mswjs/data';
 
 const db = factory({
   users: {
-    id: primaryKey(String),
+    id: primaryKey(Number),
     email: String,
     first_name: String,
     last_name: String,
@@ -11,31 +11,31 @@ const db = factory({
 
 // Default Users
  db.users.create({
-  id: '1',
+  id: 1,
   email: 'john1@ubiwhere.com',
   first_name: 'John',
   last_name: 'Doe1',
 });
 db.users.create({
-  id: '2',
+  id: 2,
   email: 'john2@ubiwhere.com',
   first_name: 'John',
   last_name: 'Doe2',
 });
 db.users.create({
-  id: '3',
+  id: 3,
   email: 'john3@ubiwhere.com',
   first_name: 'John',
   last_name: 'Doe3',
 });
 db.users.create({
-  id: '4',
+  id: 4,
   email: 'john4@ubiwhere.com',
   first_name: 'John',
   last_name: 'Doe4',
 });
 db.users.create({
-  id: '5',
+  id: 5,
   email: 'john5@ubiwhere.com',
   first_name: 'John5',
   last_name: 'Doe5',
